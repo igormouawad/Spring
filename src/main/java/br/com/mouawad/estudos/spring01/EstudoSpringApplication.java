2d9a61162ceb9c865a020d664633a3a23ef9b5c1
Igor M. Mouawad <igor@mouawad.com.br>
2021-02-23 15:39:03 -0300

    Criando operação de instanciação

--- src/main/java/br/com/mouawad/estudos/spring01/EstudoSpringApplication.java
+++ src/main/java/br/com/mouawad/estudos/spring01/EstudoSpringApplication.java
@@ -1,13 +1,32 @@
 package br.com.mouawad.estudos.spring01;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudoSpringApplication {
+import br.com.mouawad.estudos.spring01.domain.Categoria;
+import br.com.mouawad.estudos.spring01.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EstudoSpringApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
--- src/main/java/br/com/mouawad/estudos/spring01/domain/Categoria.java
+++ src/main/java/br/com/mouawad/estudos/spring01/domain/Categoria.java
@@ -1,7 +1,12 @@
 package br.com.mouawad.estudos.spring01.domain;
 
 import java.io.Serializable;
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Entity
 public class Categoria implements Serializable {
