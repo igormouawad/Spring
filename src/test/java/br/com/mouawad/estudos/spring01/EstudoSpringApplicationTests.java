1b5081575e595afaaae202d8668fb59661c0314c
Igor M. Mouawad <igor@mouawad.com.br>
2021-02-23 09:38:54 -0300

    Projeto Criado

---
+++ src/test/java/br/com/mouawad/estudos/spring01/EstudoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mouawad.estudos.spring01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
