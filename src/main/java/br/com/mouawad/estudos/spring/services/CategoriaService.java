cf343aa15b394c4e987d03f3def50eb9fdd0adee
Igor M. Mouawad <igor@mouawad.com.br>
2021-02-26 14:53:18 -0300

    Spring 'Page' paginação com paramentros opcionais na requisição

--- src/main/java/br/com/mouawad/estudos/spring/services/CategoriaService.java
+++ src/main/java/br/com/mouawad/estudos/spring/services/CategoriaService.java
@@ -5,6 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import br.com.mouawad.estudos.spring.domain.Categoria;
@@ -48,6 +52,11 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pagRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pagRequest);
 	}
 
 }
