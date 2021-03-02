bc84b8b7312975b363fa6a00b6f02faa57bd9f01
Igor M. Mouawad <igor@mouawad.com.br>
2021-03-02 12:26:44 -0300

    Configuração inicial do Spring Security

--- src/main/java/br/com/mouawad/estudos/spring/config/DevConfig.java
+++ src/main/java/br/com/mouawad/estudos/spring/config/DevConfig.java
@@ -1,31 +1,43 @@
-package br.com.mouawad.estudos.spring;
+package br.com.mouawad.estudos.spring.config;
 
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.mouawad.estudos.spring.services.DBService;
 import br.com.mouawad.estudos.spring.services.EmailService;
-import br.com.mouawad.estudos.spring.services.MockEmailService;
+import br.com.mouawad.estudos.spring.services.SmtpEmailService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
 	
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	
 	@Bean
 	public boolean instantiateDataBase() throws ParseException {
+		if(!strategy.equals("create")) {
+			return false;
+		}
 		dbService.instatiateTestDatabase();
 		return true;
 	}
 	
+	
+	
 	@Bean
 	public EmailService emailService() {
-		return new MockEmailService();
+		return new SmtpEmailService();
 	}
+	
 }
--- src/main/java/br/com/mouawad/estudos/spring/config/TestConfig.java
+++ src/main/java/br/com/mouawad/estudos/spring/config/TestConfig.java
@@ -1,43 +1,31 @@
-package br.com.mouawad.estudos.spring;
+package br.com.mouawad.estudos.spring.config;
 
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.mouawad.estudos.spring.services.DBService;
 import br.com.mouawad.estudos.spring.services.EmailService;
-import br.com.mouawad.estudos.spring.services.SmtpEmailService;
+import br.com.mouawad.estudos.spring.services.MockEmailService;
 
 @Configuration
-@Profile("dev")
-public class DevConfig {
+@Profile("test")
+public class TestConfig {
 	
 	@Autowired
 	private DBService dbService;
 	
-	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategy;
-	
-	
 	@Bean
 	public boolean instantiateDataBase() throws ParseException {
-		if(!strategy.equals("create")) {
-			return false;
-		}
 		dbService.instatiateTestDatabase();
 		return true;
 	}
 	
-	
-	
 	@Bean
 	public EmailService emailService() {
-		return new SmtpEmailService();
+		return new MockEmailService();
 	}
-	
 }
